c842d2c84a79af980c9629100c85675c3bece6ac
mohamedshalaby <shalabymohamed578@mail.com>
2025-02-15 07:37:10 +0200

    save

--- lib/ecommerce_app.dart
+++ lib/ecommerce_app.dart
@@ -18,7 +18,7 @@ class EcommerceApp extends StatelessWidget {
         },
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
-          initialRoute: Routes.mainNavigationBar,
+          initialRoute: Routes.onboardingScreen,
           onGenerateRoute: appRouter.generteRouter,
           theme: ThemeData(
             scaffoldBackgroundColor: Colors.white,
